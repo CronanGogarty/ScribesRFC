755a61c0af07dc279c2f7efa1869378bef3a4637
Cronán Gogarty <cronangogarty@outlook.com>
2016-10-10 20:40:21 -0700

    Fixed rollovers on homepage - CSS for Wordpress posts on homepage

---
+++ ScribesRFC.com/Scripts/Scribes.js
@@ -0,0 +1,33 @@
+﻿(function () {
+    console.log("Scribes.js running...");
+
+    var myPath = "http://localhost:53600/";
+
+    //$('#grid-mini-rugby').mouseover(function () {
+    //    $('#img-grid-mini')
+    //        .attr("src", myPath + "Content/Images/Image-Grid/mini-rugby-vibrant-360.png");
+    //})
+    //.mouseout(function () {
+    //    $('#img-grid-mini')
+    //        .attr("src", myPath + "Content/Images/Image-Grid/mini-rugby-greyscale-720.png");
+    //});
+
+
+    //nasty rollover code - do not use in production environment
+    //$('.gridItem').mouseover(function () {
+    //    var currentImg = $(this).find('img')
+    //    var originalImgSrc = new String(currentImg[0].currentSrc);
+    //    var hoverImgSrc = originalImgSrc.replace('greyscale', 'vibrant');
+    //    currentImg
+    //        .attr("src", hoverImgSrc);
+    //})
+    //    .mouseout(function () {
+    //        var currentImg = $(this).find('img')
+    //        var originalImgSrc = new String(currentImg[0].currentSrc);
+    //        var hoverImgSrc = originalImgSrc.replace('vibrant', 'greyscale');
+    //        currentImg
+    //            .attr("src", hoverImgSrc);
+    //    });
+
+
+}());
