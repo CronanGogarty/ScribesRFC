e78d0ff40233a462f4859933bff249cff3072b67
Cronán Gogarty <cronangogarty@outlook.com>
2016-11-09 19:49:46 -0800

    Created ASP.NET MVC HomeController and views.

--- ScribesRFC.com/Controllers/HomeController.cs
+++ ScribesRFC.com/Controllers/HomeController.cs
@@ -11,6 +11,44 @@ namespace ScribesRFC.com.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.Title = "Scribes Rugby";
+            ViewBag.NavHome = "class=active";
+            return View();
+        }        
+
+        public ActionResult JoinScribes()
+        {
+            ViewBag.NavJoin = "class=active";
+            return View();
+        }
+
+        public ActionResult LatestNews()
+        {
+            ViewBag.NavNews = "class=active";
+            return View();
+        }
+
+        public ActionResult MensRugby(){
+            ViewBag.NavMens = "class=active";
+            return View();
+        }
+
+
+        public ActionResult WomensRugby()
+        {
+            ViewBag.NavWomens = "class=active";
+            return View();
+        }
+
+        public ActionResult MinisRugby()
+        {
+            ViewBag.NavMinis = "class=active";
+            return View();
+        }
+
+        public ActionResult TagRugby()
+        {
+            ViewBag.NavTag = "class=active";
             return View();
         }
     }
