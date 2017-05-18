7734697f1d51b7eb0fd98c0a8ee7d7ef2d92aa89
Cronán Gogarty <cronangogarty@outlook.com>
2017-05-17 23:23:55 -0700

    Responsive posts with pagination
    Added some responsive code to show WP posts in responsive layouts.
    Added pagination and AJAX calls based on page.
    
    ISSUE: Not all posts share the same media properties.
    Need to test for properties prior to attempting to retrieve media.

--- ScribesRFC.com/Scripts/Scribes.js
+++ ScribesRFC.com/Scripts/Scribes.js
@@ -8,24 +8,27 @@
 
     var categories = [];
     var posts = [];
+    var pageNumber = 1;
 
     function loadPosts() {
 
-        //stor the markup in variables
-        
+        //store the markup in variables
 
         console.log('calling WP Posts...');
 
         $.ajax({
             method: 'GET',
-            url: 'http://www.scribesrfc.com/wp-json/wp/v2/posts?_embed&filter[per_page]=20',
+            url: 'http://www.scribesrfc.com/wp-json/wp/v2/posts?_embed&per_page=5&page=' + pageNumber,
             dataType: 'json'
         })
         .always(function () {
             console.log('WP Done!');
         })
-        .done(function (data) {
+        .done(function (data, textStatus, jqXHR) {
             posts = data;
+
+            let totalPages = parseInt(jqXHR.getResponseHeader('X-WP-TotalPages'), 10);
+            console.log('totalPages = ' + totalPages);
 
             //get the most recent post
             var latestPost = data[0];
@@ -41,17 +44,19 @@
                     categoriesHTML += '<a href="' + categories[i].link + '" rel="category tag">' + categories[i].name + '</a> &#8226; ';
                 }
             }
-            var latestPostHTML = '<article id="paged-pro-feature" class="post-' + latestPost.id + ' post type-post status-publish format-standard has-post-thumbnail hentry category-news category-social-events"><div class="container-blog"><div class="hover-feature-pro"><div class="featured-image-pro"><a href="' + latestPost.link + '" class="video-hover-pro"><img src="' + featuredMedia + '" class="attachment-progression-blog-single size-progression-blog-single wp-post-image" alt=""></a></div><div class="feature-text-pro"><div class="category-list-pro">' + categoriesHTML + '</div><h2 class="blog-title-pro"><a href="' + latestPost.link + '">' + latestPost.title.rendered + '</a></h2><div class="time-stamp-pro">' + new Date(latestPost.date).toDateString() + '</div><div class="clearfix"></div></div></div></div></article>'
+            var latestPostHTML = '<article id="paged-pro-feature" class="post-' + latestPost.id + ' post type-post status-publish format-standard has-post-thumbnail hentry category-news category-social-events"><div class="container-blog"><div class="hover-feature-pro"><div class="featured-image-pro"><a href="' + latestPost.link + '" class="video-hover-pro"><img src="' + featuredMedia + '" class="attachment-progression-blog-single size-progression-blog-single wp-post-image img-responsive" style="width:100vw" alt=""></a></div><div class="feature-text-pro"><div class="category-list-pro">' + categoriesHTML + '</div><h2 class="blog-title-pro"><a href="' + latestPost.link + '">' + latestPost.title.rendered + '</a></h2><div class="time-stamp-pro">' + new Date(latestPost.date).toDateString() + '</div><div class="clearfix"></div></div></div></div></article>'
             //now apend to the appropriate location in the home/index view
             document.getElementById('paged-index-pro').innerHTML = latestPostHTML;
 
-            //get all the other posts and output top 6 to the home/index view
-            var topSixPosts = data.slice(1, 9); //create a new array with 6 elements
-            var topSixPostsHTML = '';
-            for (let i = 0; i < length; i++) {
-                featuredMedia = topSixPosts[i]._embedded['wp:featuredmedia'][0].media_details.sizes['progression-blog'].source_url;
+            //output next 4 posts to the home/index view
+            var topFourPosts = data.slice(1, 5); //create a new array with 4 elements
+            var topFourPostsHTML = '';
+            for (let i = 0; i < topFourPosts.length; i++) {
+                //TODO:older posts do not have the media_details.sizes['progression-blog'] properties... figure a way to show old and new posts that all show images
+                featuredMedia = topFourPosts[i]._embedded['wp:featuredmedia'][0].media_details.sizes['progression-blog'].source_url;
+                //featuredMedia = topFourPosts[i]._embedded['wp:featuredmedia'][0].media_details.source_url;
                 categoriesHTML = '';
-                categories = topSixPosts[i]._embedded['wp:term'][0];
+                categories = topFourPosts[i]._embedded['wp:term'][0];
                 for (let j = 0; j < categories.length; j++) {
                     if (j == categories.length - 1) {
                         //if this is the last item we don't want to append the bullet point
@@ -61,16 +66,51 @@
                         categoriesHTML += '<a href="' + categories[j].link + '" rel="category tag">' + categories[j].name + '</a> &#8226; ';
                     }
                 }
+
                 var isLastColumn = (i % 2 != 0) ? "lastcolumn-progression" : null;
-                topSixPostsHTML += '<div class="infinite-container"><div class="grid2column-progression ' + isLastColumn +'"><article id="post-' + topSixPosts[i].id + '" class="post-' + topSixPosts[i].id + ' post type-post status-publish format-standard has-post-thumbnail hentry category-mens-rugby category-womens-rugby"><div class="container-blog"><div class="featured-image-pro"><a href="' + topSixPosts[i].link + '" class="video-hover-pro"><img src="' + featuredMedia + '" class="attachment-progression-blog size-progression-blog wp-post-image" alt=""></a></div><div class="category-list-pro">' + categoriesHTML + '</div><h2 class="blog-title-pro"><a href="' + topSixPosts[i].link + '">' + topSixPosts[i].title.rendered + '</a></h2><div class="time-stamp-pro">' + new Date(topSixPosts[i].date).toDateString() + '</div><div class="clearfix"></div></div></article></div></div>'
+                topFourPostsHTML += '<div class="infinite-container"><div class="grid2column-progression ' + isLastColumn +'"><article id="post-' + topFourPosts[i].id + '" class="post-' + topFourPosts[i].id + ' post type-post status-publish format-standard has-post-thumbnail hentry category-mens-rugby category-womens-rugby"><div class="container-blog"><div class="featured-image-pro"><a href="' + topFourPosts[i].link + '" class="video-hover-pro"><img src="' + featuredMedia + '" class="attachment-progression-blog size-progression-blog wp-post-image" alt=""></a></div><div class="category-list-pro">' + categoriesHTML + '</div><h2 class="blog-title-pro"><a href="' + topFourPosts[i].link + '">' + topFourPosts[i].title.rendered + '</a></h2><div class="time-stamp-pro">' + new Date(topFourPosts[i].date).toDateString() + '</div><div class="clearfix"></div></div></article></div></div>'
             }
 
-            document.getElementById('infinite-pro').innerHTML = topSixPostsHTML;
+            topFourPostsHTML += '<div class="clearfix"></div>';
+
+            document.getElementById('infinite-pro').innerHTML = topFourPostsHTML;
+
+            //create the pagination buttons
+            var paginationButtons = '';
+            console.log('pagination : pageNumber = ' + pageNumber);
+            if (pageNumber > 1) {
+                paginationButtons += '<li><span class="page-numbers" data-page-number="1">‹‹ First</span></li>';
+            }
+
+            if (pageNumber == 1) {
+                paginationButtons += '<li><span class="page-numbers current" data-page-number="' + (pageNumber) + '">' + (pageNumber) + '</span></li><li><span class="page-numbers" data-page-number="' + (pageNumber + 1) + '">' + (pageNumber + 1) + '</span></li><li><span class="page-numbers" data-page-number="' + (pageNumber + 2) + '">' + (pageNumber + 2) + '</span></li>'
+            }
+
+            if (pageNumber != 1 && pageNumber != totalPages) {
+                paginationButtons += '<li><span class="page-numbers" data-page-number="' + (pageNumber - 1) + '">' + (pageNumber - 1) + '</span></li><li><span class="page-numbers current" data-page-number="' + (pageNumber) + '">' + (pageNumber) + '</span></li><li><span class="page-numbers" data-page-number="' + (pageNumber + 1) + '">' + (pageNumber + 1) + '</span></li>'
+            }
+
+            if (pageNumber < totalPages) {
+                paginationButtons += '<span class="page-numbers" data-page-number="' + totalPages + '">Last ››</span>';
+            }
+
+            if (pageNumber == totalPages) {
+                paginationButtons += '<li><span class="page-numbers" data-page-number="1">‹‹ First</span></li><li><span class="page-numbers" data-page-number="' + (pageNumber - 2) + '">' + (pageNumber - 2) + '</span></li><li><span class="prev page-numbers" data-page-number="' + (pageNumber - 1) + '">' + (pageNumber - 1) + '</span></li><li><span class="page-numbers current" data-page-number="' + (pageNumber) + '">' + (pageNumber) + '</span></li>'
+            }
+
+            document.getElementById('ul-page-numbers').innerHTML = paginationButtons;
         })
         .fail(function (jqXHR, textStatus) {
             console.log('WP Fail! - ' + textStatus);
         })
     }
+
+    $('#ul-page-numbers').on('click', '.page-numbers', function () {
+        console.log('click...');
+        let $this = $(this);
+        pageNumber = (parseInt($this.attr('data-page-number'), 10));
+        loadPosts();
+    });
 
     //function loadCategories() {
 
